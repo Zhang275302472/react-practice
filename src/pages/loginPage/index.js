708c6ef20414b8f85cfe3c1968fdd2a1ceebd6a4
张皓然 <zhanghaoran@zhanghaorandeMacBook-Pro.local>
2020-02-27 12:03:31 +0800

    删除debugger

--- src/pages/loginPage/index.js
+++ src/pages/loginPage/index.js
@@ -5,15 +5,27 @@ import { FetchData } from '@/redux/actions/login-actions.js'
 class LoginPages extends React.Component {
     constructor(props, context) {
         super(props)
+        // ES6写法，也就是类(Class)的写法，React默认不会自动绑定类中的方法造成的
+        // this.abs = this.abs.bind(this)
+        this.state = {
+            name : 'qweqwe'
+        }
     }
     componentDidMount() {
         this.props.login({userName: 'zhanghaoran'})
+        // this.setState({name: 'aaass'})
+    }
+
+    setData = (e) =>{
+        console.log('e', e)
+        this.setState({name: 'aaa'})
     }
 
     render() {
         return (
-            <div>
+            <div onClick={this.setData}>
                 登陆页
+                <p>{this.state.name}</p>
             </div>
         )
     }
@@ -38,3 +50,15 @@ const LoginPage = connect(
     mapDispatchToProps
 )(LoginPages)
 export default LoginPage
+
+
+
+// ES6写法，也就是类(Class)的写法，React默认不会自动绑定类中的方法造成的
+// this.abs = this.abs.bind(this)
+// abs(a) {}
+
+// 或者
+// abs = (e) =>{
+//     console.log('e', e)
+//     this.setState({name: 'aaa'})
+// }
